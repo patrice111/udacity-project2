087bf4e296d27144e65299e258905835bad9c33a
Patrice Codes <42042567+patrice111@users.noreply.github.com>
2020-12-14 14:26:22 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,8 +4,6 @@ const navOpen = document.querySelector(".hamburger");
 const navClose = document.querySelector(".close");
 const navBar = document.querySelector(".nav");
 
-
-
 const navLeft = menu.getBoundingClientRect().left;
 
 navOpen.addEventListener("click", () => {
@@ -39,9 +37,9 @@ window.addEventListener("scroll", () => {
 //Scroll
 
 const links = [...document.querySelectorAll(".scroll-link")];
-links.map(link => {
+links.map((link) => {
     if (!link) return;
-    link.addEventListener("click", e => {
+    link.addEventListener("click", (e) => {
         e.preventDefault();
 
         const id = e.scrollLink.getAttribute("href").slice(1);
@@ -61,74 +59,29 @@ links.map(link => {
     });
 });
 
-gsap.from(".logo", {
-    opacity: 0,
-    duration: 1,
-    delay: 0.5,
-    y: -10
-});
-gsap.from(".hamburger", {
-    opacity: 0,
-    duration: 1,
-    delay: 1,
-    x: 20
-});
-gsap.from(".hero-img", {
-    opacity: 0,
-    duration: 1,
-    delay: 1.5,
-    x: -200
-});
-gsap.from(".hero-content h2", {
-    opacity: 0,
-    duration: 1,
-    delay: 2,
-    y: -50
-});
-gsap.from(".hero-content h1", {
-    opacity: 0,
-    duration: 1,
-    delay: 2.5,
-    y: -45
-});
-gsap.from(".hero-content a", {
-    opacity: 0,
-    duration: 1,
-    delay: 3.5,
-    y: 50
-});
-
-
-
-//Active State
 //Change navigation style on scroll
 let mainNavLinks = document.querySelectorAll(".nav-list a");
 
-window.addEventListener("scroll", event => {
-  let fromTop = window.scrollY;
+window.addEventListener("scroll", (event) => {
+    let fromTop = window.scrollY;
 
-  mainNavLinks.forEach(link => {
-    let section = document.querySelector(link.hash);
+    mainNavLinks.forEach((link) => {
+        let section = document.querySelector(link.hash);
 
-    if (
-      section.offsetTop <= fromTop &&
-      section.offsetTop + section.offsetHeight > fromTop
-    ) {
-      link.classList.add("active");
-    } else {
-      link.classList.remove("active");
-    }
-  });
+        if (section.offsetTop <= fromTop && section.offsetTop + section.offsetHeight > fromTop) {
+            link.classList.add("active");
+        } else {
+            link.classList.remove("active");
+        }
+    });
 });
 
-
-
 //Top Button
-var mybutton = document.getElementById("myBtn");
+const mybutton = document.getElementById("myBtn");
 
 // Once user scrolls down 10px from top show button
-window.onscroll = function() {
-    scrollFunction()
+window.onscroll = function () {
+    scrollFunction();
 };
 
 function scrollFunction() {
@@ -137,10 +90,10 @@ function scrollFunction() {
     } else {
         mybutton.style.display = "none";
     }
-};
+}
 
-//Once clicked button scrolls to top 
+//Once clicked button scrolls to top
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
-};
+}
